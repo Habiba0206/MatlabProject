50f31abd64c0002ea548452701dd4007370fa063
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-07 00:23:41 +0500

    Created notification configurations

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/EntityConfigurations/EmailTemplateConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using MatlabProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CourseProject.Persistence.EntityConfigurations;
+
+public class EmailTemplateConfiguration : IEntityTypeConfiguration<EmailTemplate>
+{
+    public void Configure(EntityTypeBuilder<EmailTemplate> builder)
+    {
+        builder.Property(template => template.Subject).IsRequired().HasMaxLength(256);
+    }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/EntityConfigurations/NotificationTemplateConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using MatlabProject.Domain.Entities;
+using MatlabProject.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CourseProject.Persistence.EntityConfigurations;
+
+public class NotificationTemplateConfiguration : IEntityTypeConfiguration<NotificationTemplate>
+{
+    public void Configure(EntityTypeBuilder<NotificationTemplate> builder)
+    {
+        builder.Property(template => template.Content).HasMaxLength(129_536);
+
+        builder.HasIndex(
+                template => new
+                {
+                    template.Type,
+                    template.TemplateType
+                }
+            )
+            .IsUnique();
+
+        builder.ToTable("NotificationTemplates")
+            .HasDiscriminator(emailTemplate => emailTemplate.Type)
+            .HasValue<EmailTemplate>(NotificationType.Email);
+    }
+}
