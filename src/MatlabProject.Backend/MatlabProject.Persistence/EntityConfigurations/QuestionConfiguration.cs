fb18c27c39ffb9d5b5ca83c062f81f359c634fe6
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-08 01:23:39 +0500

    Created question configuration

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/EntityConfigurations/QuestionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using MatlabProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MatlabProject.Persistence.EntityConfigurations;
+
+public class QuestionConfiguration : IEntityTypeConfiguration<Question>
+{
+    public void Configure(EntityTypeBuilder<Question> builder)
+    {
+        builder.Property(q => q.Text)
+            .IsRequired();
+
+        builder.Property(q => q.CorrectAnswer)
+            .IsRequired();
+
+        builder.HasMany(q => q.AnswerOptions)
+            .WithOne(o => o.Question)
+            .HasForeignKey(o => o.QuestionId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
