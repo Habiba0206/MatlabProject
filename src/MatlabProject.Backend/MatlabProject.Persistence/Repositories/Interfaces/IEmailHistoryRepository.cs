ffd00c59a039f49a926652974647147d173deaa6
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-08 01:21:23 +0500

    Created notification result repository

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/Repositories/Interfaces/IEmailHistoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace MatlabProject.Persistence.Repositories.Interfaces;
+
+public interface IEmailHistoryRepository
+{
+    IQueryable<EmailHistory> Get(
+             Expression<Func<EmailHistory, bool>>? predicate = default,
+             QueryOptions queryOptions = default);
+
+    ValueTask<EmailHistory?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<IList<EmailHistory>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<EmailHistory> CreateAsync(
+        EmailHistory emailHistory,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/Repositories/Interfaces/IEmailTemplateRepository.cs
@@ -0,0 +1,33 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace MatlabProject.Persistence.Repositories.Interfaces;
+
+public interface IEmailTemplateRepository
+{
+    IQueryable<EmailTemplate> Get(
+             Expression<Func<EmailTemplate, bool>>? predicate = default,
+             QueryOptions queryOptions = default);
+
+    ValueTask<EmailTemplate?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<IList<EmailTemplate>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<EmailTemplate> CreateAsync(
+        EmailTemplate emailTemplate,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+}
+
