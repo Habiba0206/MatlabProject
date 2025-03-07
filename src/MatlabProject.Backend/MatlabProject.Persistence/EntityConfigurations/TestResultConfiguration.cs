5f068c8ac5d9b666f2006f2aac3f632e24e22601
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-08 01:23:19 +0500

    Created test result configuration

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/EntityConfigurations/TestResultConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using MatlabProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MatlabProject.Persistence.EntityConfigurations;
+
+public class TestResultConfiguration : IEntityTypeConfiguration<TestResult>
+{
+    public void Configure(EntityTypeBuilder<TestResult> builder)
+    {
+        builder.Property(tr => tr.ScorePercentage)
+            .IsRequired();
+
+        builder.Property(tr => tr.Passed)
+            .HasDefaultValue(false);
+
+        builder.HasOne(tr => tr.User)
+            .WithMany(u => u.TestResults)
+            .HasForeignKey(tr => tr.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(tr => tr.Test)
+            .WithMany()
+            .HasForeignKey(tr => tr.TestId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
