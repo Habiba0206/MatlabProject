85d773d209a15110883f74bd2b250721a81a97f3
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-08 01:22:03 +0500

    Created access token repository

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/Repositories/Interfaces/IAccessTokenRepository.cs
@@ -0,0 +1,18 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Entities;
+
+namespace MatlabProject.Persistence.Repositories.Interfaces;
+
+public interface IAccessTokenRepository
+{
+    ValueTask<AccessToken> CreateAsync(
+        AccessToken accessToken,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default);
+    ValueTask<AccessToken?> GetByIdAsync(
+        Guid accessTokeId,
+        CancellationToken cancellationToken = default);
+    ValueTask<AccessToken> UpdateAsync(
+        AccessToken accessToken,
+        CancellationToken cancellationToken = default);
+}
