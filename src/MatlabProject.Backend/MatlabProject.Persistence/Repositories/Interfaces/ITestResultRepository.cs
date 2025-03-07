a84c3c42d53bfbc605f0ce2538fddc3a4225a939
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-08 01:20:50 +0500

    Created test result repository

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/Repositories/Interfaces/ITestResultRepository.cs
@@ -0,0 +1,47 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace MatlabProject.Persistence.Repositories.Interfaces;
+
+public interface ITestResultRepository
+{
+    IQueryable<TestResult> Get(
+             Expression<Func<TestResult, bool>>? predicate = default,
+             QueryOptions queryOptions = default);
+
+    ValueTask<TestResult?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<IList<TestResult>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<TestResult> CreateAsync(
+        TestResult testResult,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<TestResult> UpdateAsync(
+        TestResult testResult,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<TestResult?> DeleteAsync(
+        TestResult testResult,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<TestResult?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+}
