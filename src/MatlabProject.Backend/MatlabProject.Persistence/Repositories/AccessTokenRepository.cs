60cc90598aa62773e0b0dec42cb21d053f50f536
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-08 01:22:29 +0500

    Implemented access token repository

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/Repositories/AccessTokenRepository.cs
@@ -0,0 +1,31 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Entities;
+using MatlabProject.Persistence.Caching.Brokers;
+using MatlabProject.Persistence.Caching.Models;
+using MatlabProject.Persistence.Repositories.Interfaces;
+
+namespace MatlabProject.Persistence.Repositories;
+
+public class AccessTokenRepository(ICacheBroker cacheBroker) : IAccessTokenRepository
+{
+    public async ValueTask<AccessToken> CreateAsync(AccessToken accessToken, CommandOptions commandOptions, CancellationToken cancellationToken = default)
+    {
+        var cacheEntryOptions = new CacheEntryOptions(accessToken.ExpiryTime - DateTimeOffset.UtcNow, null);
+        await cacheBroker.SetAsync(accessToken.Id.ToString(), accessToken, cacheEntryOptions, cancellationToken);
+
+        return accessToken;
+    }
+
+    public ValueTask<AccessToken?> GetByIdAsync(Guid accessTokenId, CancellationToken cancellationToken = default)
+    {
+        return cacheBroker.GetAsync<AccessToken>(accessTokenId.ToString(), cancellationToken);
+    }
+
+    public async ValueTask<AccessToken> UpdateAsync(AccessToken accessToken, CancellationToken cancellationToken = default)
+    {
+        var cacheEntryOptions = new CacheEntryOptions(accessToken.ExpiryTime - DateTimeOffset.UtcNow, null);
+        await cacheBroker.SetAsync(accessToken.Id.ToString(), accessToken, cacheEntryOptions, cancellationToken);
+
+        return accessToken;
+    }
+}
