c07dd1ee2cf46088a62edc92de36b96ac2cc5aa9
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-08 01:24:24 +0500

    Created answer option configuration

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/EntityConfigurations/AnswerOptionConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using MatlabProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MatlabProject.Persistence.EntityConfigurations;
+
+public class AnswerOptionConfiguration : IEntityTypeConfiguration<AnswerOption>
+{
+    public void Configure(EntityTypeBuilder<AnswerOption> builder)
+    {
+        builder.Property(o => o.Text)
+            .IsRequired();
+    }
+}
