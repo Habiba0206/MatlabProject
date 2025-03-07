21e1522763cf1ca603eee04515289ea76b120870
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-08 01:23:30 +0500

    Created test configuration

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/EntityConfigurations/TestConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using MatlabProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MatlabProject.Persistence.EntityConfigurations;
+
+public class TestConfiguration : IEntityTypeConfiguration<Test>
+{
+    public void Configure(EntityTypeBuilder<Test> builder)
+    {
+        builder.Property(t => t.Title)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        builder.HasMany(t => t.Questions)
+            .WithOne(q => q.Test)
+            .HasForeignKey(q => q.TestId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
