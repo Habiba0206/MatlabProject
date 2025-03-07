2990d91a918b2d5aba5d66eb20c457c6300742f2
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-08 01:20:18 +0500

    Created user and user settings repository

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/Repositories/Interfaces/IUserSettingsRepository.cs
@@ -0,0 +1,20 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace MatlabProject.Persistence.Repositories.Interfaces;
+
+public interface IUserSettingsRepository
+{
+    ValueTask<UserSettings?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<UserSettings> CreateAsync(
+        UserSettings userSettings,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+}
