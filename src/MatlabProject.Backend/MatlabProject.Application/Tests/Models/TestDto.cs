47670d6a3e51bb7317ca534de8f5f0c8bc78aff2
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 01:39:49 +0500

    Created test and test result dto models

---
+++ src/MatlabProject.Backend/MatlabProject.Application/Tests/Models/TestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MatlabProject.Application.Tests.Models;
+
+public class TestDto
+{
+    public Guid? Id { get; set; }
+    public string Title { get; set; }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Application/Tests/Models/TestResultDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MatlabProject.Application.Tests.Models;
+
+public class TestResultDto
+{
+    public Guid? Id { get; set; }
+    public Guid FormId { get; set; }
+    public Guid QuestionId { get; set; }
+    public string Value { get; set; }
+}
