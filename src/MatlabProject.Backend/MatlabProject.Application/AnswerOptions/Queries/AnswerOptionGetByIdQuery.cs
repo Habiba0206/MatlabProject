79a0c48cdaaa8f9bb38146f9306bd977a6796239
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 02:48:32 +0500

    Created answer option queries

---
+++ src/MatlabProject.Backend/MatlabProject.Application/AnswerOptions/Queries/AnswerOptionGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Application.AnswerOptions.Models;
+
+namespace MatlabProject.Application.AnswerOptions.Queries;
+
+public class AnswerOptionGetByIdQuery : IQuery<AnswerOptionDto?>
+{
+    public Guid AnswerOptionId { get; set; }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Application/AnswerOptions/Queries/AnswerOptionGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Application.AnswerOptions.Models;
+
+namespace MatlabProject.Application.AnswerOptions.Queries;
+
+public class AnswerOptionGetQuery : IQuery<ICollection<AnswerOptionDto>>
+{
+    public AnswerOptionFilter AnswerOptionFilter { get; set; }
+}
