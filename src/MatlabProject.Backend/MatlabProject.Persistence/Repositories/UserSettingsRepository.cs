7a3b951e20399f4b8bb503eb1896459d78ab9777
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 01:23:15 +0500

    Implemented user settings repository

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/Repositories/UserSettingsRepository.cs
@@ -0,0 +1,27 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Domain.Entities;
+using MatlabProject.Persistence.Caching.Brokers;
+using MatlabProject.Persistence.DataContexts;
+using MatlabProject.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace MatlabProject.Persistence.Repositories;
+
+public class UserSettingsRepository(AppDbContext appDbContext, ICacheBroker cacheBroker) :
+    EntityRepositoryBase<UserSettings, AppDbContext>(appDbContext, cacheBroker),
+    IUserSettingsRepository
+
+{
+    public ValueTask<UserSettings?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<UserSettings> CreateAsync(
+        UserSettings userSettings,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.CreateAsync(userSettings, commandOptions, cancellationToken);
+}
