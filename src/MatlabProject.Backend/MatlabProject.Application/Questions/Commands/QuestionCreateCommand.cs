34362028000382d721fe3d81ad8b148d0327c31d
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 02:25:40 +0500

    Created question commands

---
+++ src/MatlabProject.Backend/MatlabProject.Application/Questions/Commands/QuestionCreateCommand.cs
@@ -0,0 +1,9 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Application.Questions.Models;
+
+namespace MatlabProject.Application.Questions.Commands;
+
+public record QuestionCreateCommand : ICommand<QuestionDto>
+{
+    public QuestionDto QuestionDto { get; set; }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Application/Questions/Commands/QuestionDeleteByIdCommand.cs
@@ -0,0 +1,8 @@
+﻿using MatlabProject.Domain.Common.Commands;
+
+namespace MatlabProject.Application.Questions.Commands;
+
+public class QuestionDeleteByIdCommand : ICommand<bool>
+{
+    public Guid QuestionId { get; set; }
+}
