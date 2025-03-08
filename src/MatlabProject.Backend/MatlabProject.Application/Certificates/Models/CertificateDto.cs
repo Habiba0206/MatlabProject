06dba1e8d88adc5a8bdaf649f2f9749de0e5f308
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 02:46:18 +0500

    Created certificate dto and filter models

---
+++ src/MatlabProject.Backend/MatlabProject.Application/Certificates/Models/CertificateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MatlabProject.Application.Certificates.Models;
+
+public class CertificateDto
+{
+    public Guid? Id { get; set; }
+    public Guid UserId { get; set; }
+    public Guid TestId { get; set; }
+    public DateTimeOffset IssuedAt { get; set; }
+    public string FilePath { get; set; } = default!;
+}
