e6a9f47953dc53fe7e324bce5521bbb8761f1373
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 02:45:42 +0500

    Created event bus broker

---
+++ src/MatlabProject.Backend/MatlabProject.Application/Common/EventBus/Brokers/IEventBusBroker.cs
@@ -0,0 +1,16 @@
+﻿using MatlabProject.Domain.Common.Events;
+
+namespace MatlabProject.Application.Common.EventBus.Brokers;
+
+public interface IEventBusBroker
+{
+    ValueTask PublishLocalAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase;
+
+    ValueTask PublishAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase;
+}
