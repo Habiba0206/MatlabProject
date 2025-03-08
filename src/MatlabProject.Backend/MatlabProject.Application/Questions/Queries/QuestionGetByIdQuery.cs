c7540ecbfd3ac095ddb4103f9906f8dde3f6ae68
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 01:59:12 +0500

    Created question queries

---
+++ src/MatlabProject.Backend/MatlabProject.Application/Questions/Queries/QuestionGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Application.Questions.Models;
+
+namespace MatlabProject.Application.Questions.Queries;
+
+public class QuestionGetByIdQuery : IQuery<QuestionDto?>
+{
+    public Guid QuestionId { get; set; }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Application/Questions/Queries/QuestionGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Application.Questions.Models;
+
+namespace MatlabProject.Application.Questions.Queries;
+
+public class QuestionGetQuery : IQuery<ICollection<QuestionDto>>
+{
+    public QuestionFilter QuestionFilter { get; set; }
+}
