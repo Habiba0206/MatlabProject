458bde6fcfb06f61a6bfbc0b8a6d1ea99a119edf
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 02:42:05 +0500

    Created user queries

---
+++ src/MatlabProject.Backend/MatlabProject.Application/Identity/Queries/UserGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Application.Identity.Models;
+
+namespace MatlabProject.Application.Identity.Queries;
+
+public class UserGetByIdQuery : IQuery<UserDto?>
+{
+    public Guid UserId { get; set; }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Application/Identity/Queries/UserGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Application.Identity.Models;
+
+namespace MatlabProject.Application.Identity.Queries;
+
+public class UserGetQuery : IQuery<ICollection<UserDto>>
+{
+    public UserFilter UserFilter { get; set; }
+}
