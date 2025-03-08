a4db3d868debfb61c64d5f2d5529a657143e14fc
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 02:42:17 +0500

    Created user dto and filter models

---
+++ src/MatlabProject.Backend/MatlabProject.Application/Identity/Models/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using MatlabProject.Domain.Enums;
+
+namespace MatlabProject.Application.Identity.Models;
+
+public class UserDto
+{
+    public Guid? Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public int Age { get; set; }
+    public Role Role { get; set; }
+    public UserState UserState { get; set; }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Application/Identity/Models/UserFilter.cs
@@ -0,0 +1,29 @@
+﻿using MatlabProject.Domain.Common.Queries;
+
+namespace MatlabProject.Application.Identity.Models;
+
+public class UserFilter : FilterPagination
+{
+    /// <summary>
+    /// Overrides base GetHashCode method
+    /// </summary>
+    /// <returns></returns>
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+
+        return hashCode.ToHashCode();
+    }
+
+    /// <summary>
+    /// Overrides base Equels method
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public override bool Equals(object? obj) =>
+        obj is UserFilter filter
+        && filter.GetHashCode() == GetHashCode();
+}
