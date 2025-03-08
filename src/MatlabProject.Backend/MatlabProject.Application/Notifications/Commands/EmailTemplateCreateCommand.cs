61996ea0e6cbdf18c615b9a8595fff9506f2d881
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 02:35:29 +0500

    Created email template create command

---
+++ src/MatlabProject.Backend/MatlabProject.Application/Notifications/Commands/EmailTemplateCreateCommand.cs
@@ -0,0 +1,9 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Application.Notifications.Models;
+
+namespace MatlabProject.Application.Notifications.Commands;
+
+public class EmailTemplateCreateCommand : ICommand<EmailTemplateDto>
+{
+    public EmailTemplateDto EmailTemplateDto { get; set; }
+}
