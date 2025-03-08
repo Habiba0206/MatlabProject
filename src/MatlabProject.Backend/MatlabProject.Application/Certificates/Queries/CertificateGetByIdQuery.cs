c661ff3bf3981ef27c15048ac8c014eb88633ef9
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 02:46:02 +0500

    Created certificate queries

---
+++ src/MatlabProject.Backend/MatlabProject.Application/Certificates/Queries/CertificateGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Application.Certificates.Models;
+
+namespace MatlabProject.Application.Certificates.Queries;
+
+public class CertificateGetByIdQuery : IQuery<CertificateDto?>
+{
+    public Guid CertificateId { get; set; }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Application/Certificates/Queries/CertificateGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Application.Certificates.Models;
+
+namespace MatlabProject.Application.Certificates.Queries;
+
+public class CertificateGetQuery : IQuery<ICollection<CertificateDto>>
+{
+    public CertificateFilter CertificateFilter { get; set; }
+}
