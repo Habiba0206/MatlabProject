875ff9fda1f590bdd5bbe249cc02d5d1ebe54a38
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 01:39:33 +0500

    Created test and test result filter models

---
+++ src/MatlabProject.Backend/MatlabProject.Application/Tests/Models/TestResultFilter.cs
@@ -0,0 +1,29 @@
+﻿using MatlabProject.Domain.Common.Queries;
+
+namespace MatlabProject.Application.Tests.Models;
+
+public class TestResultFilter : FilterPagination
+{
+    /// <summary>
+    /// Overrides base GetHashCode method
+    /// </summary>
+    /// <returns></returns>
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+
+        return hashCode.ToHashCode();
+    }
+
+    /// <summary>
+    /// Overrides base Equels method
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public override bool Equals(object? obj) =>
+        obj is TestResultFilter filter
+        && filter.GetHashCode() == GetHashCode();
+}
