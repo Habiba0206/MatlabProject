0595ee6477eeb8e146d839af2b1e501945d51681
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 01:24:28 +0500

    Implemented certificate repository

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/Repositories/CertificateRepository.cs
@@ -0,0 +1,61 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Domain.Entities;
+using MatlabProject.Persistence.Caching.Brokers;
+using MatlabProject.Persistence.DataContexts;
+using MatlabProject.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace MatlabProject.Persistence.Repositories;
+
+public class CertificateRepository(AppDbContext appDbContext, ICacheBroker cacheBroker) :
+    EntityRepositoryBase<Certificate, AppDbContext>(appDbContext, cacheBroker),
+    ICertificateRepository
+
+{
+    public IQueryable<Certificate> Get(
+        Expression<Func<Certificate, bool>>? predicate = null,
+        QueryOptions queryOptions = default) =>
+    base.Get(predicate, queryOptions);
+
+    public ValueTask<Certificate?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<Certificate>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+    base.CheckByIdAsync(id, cancellationToken);
+
+    public ValueTask<Certificate> CreateAsync(
+        Certificate certificate,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.CreateAsync(certificate, commandOptions, cancellationToken);
+
+    public ValueTask<Certificate> UpdateAsync(
+        Certificate certificate,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken) =>
+    base.UpdateAsync(certificate, commandOptions, cancellationToken);
+
+    public ValueTask<Certificate?> DeleteAsync(
+        Certificate certificate,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default) =>
+    base.UpdateAsync(certificate, commandOptions, cancellationToken);
+
+    public ValueTask<Certificate?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default) =>
+    base.DeleteByIdAsync(id, commandOptions, cancellationToken);
+}
