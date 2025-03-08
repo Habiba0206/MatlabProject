79c7da9550e49c8426313c2502190d474e6f70d4
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 02:25:26 +0500

    Created question dto model

---
+++ src/MatlabProject.Backend/MatlabProject.Application/Questions/Models/QuestionDto.cs
@@ -0,0 +1,11 @@
+﻿using MatlabProject.Domain.Enums;
+
+namespace MatlabProject.Application.Questions.Models;
+
+public class QuestionDto
+{
+    public Guid? Id { get; set; }
+    public Guid TestId { get; set; }
+    public string Text { get; set; }
+    public string CorrectAnswer { get; set; }
+}
