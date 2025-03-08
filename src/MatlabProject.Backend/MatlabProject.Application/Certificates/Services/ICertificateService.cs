56c37ab17a57228e2772e5943a39549bde0df495
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 02:45:52 +0500

    Creates certificate service

---
+++ src/MatlabProject.Backend/MatlabProject.Application/Certificates/Services/ICertificateService.cs
@@ -0,0 +1,52 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Domain.Entities;
+using MatlabProject.Application.Certificates.Models;
+using System.Linq.Expressions;
+
+namespace MatlabProject.Application.Certificates.Services;
+
+public interface ICertificateService
+{
+    IQueryable<Certificate> Get(
+             Expression<Func<Certificate, bool>>? predicate = default,
+             QueryOptions queryOptions = default);
+
+    IQueryable<Certificate> Get(
+        CertificateFilter certificateFilter,
+        QueryOptions queryOptions = default);
+
+    ValueTask<Certificate?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<IList<Certificate>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Certificate> CreateAsync(
+        Certificate certificate,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Certificate> UpdateAsync(
+        Certificate certificate,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Certificate?> DeleteAsync(
+        Certificate certificate,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Certificate?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+}
