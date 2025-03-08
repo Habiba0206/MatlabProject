319ad98d9388af1ccbcc98383d1ca1639ada2aad
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 02:48:50 +0500

    Created answer option dto and filter models

---
+++ src/MatlabProject.Backend/MatlabProject.Application/AnswerOptions/Models/AnswerOptionDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MatlabProject.Application.AnswerOptions.Models;
+
+public class AnswerOptionDto
+{
+    public Guid? Id { get; set; }
+    public Guid QuestionId { get; set; }
+    public string Text { get; set; } = default!;
+}
