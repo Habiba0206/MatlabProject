5561915d3e7f89d6c56c6e351e7f254d92bd388a
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-09 01:23:35 +0500

    Implemented user info verification repository

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/Repositories/UserInfoVerificationCodeRepository.cs
@@ -0,0 +1,59 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Domain.Entities;
+using MatlabProject.Persistence.Caching.Brokers;
+using MatlabProject.Persistence.DataContexts;
+using MatlabProject.Persistence.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MatlabProject.Persistence.Repositories;
+
+public class UserInfoVerificationCodeRepository(AppDbContext appDbContext, ICacheBroker cacheBroker) :
+    EntityRepositoryBase<UserInfoVerificationCode, AppDbContext>(appDbContext, cacheBroker),
+    IUserInfoVerificationCodeRepository
+
+{
+    public IQueryable<UserInfoVerificationCode> Get(
+        Expression<Func<UserInfoVerificationCode, bool>>? predicate = null,
+        QueryOptions queryOptions = default) =>
+    base.Get(predicate, queryOptions);
+
+    public ValueTask<UserInfoVerificationCode?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<UserInfoVerificationCode>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+    base.CheckByIdAsync(id, cancellationToken);
+
+    public new async ValueTask<UserInfoVerificationCode> CreateAsync(
+        UserInfoVerificationCode verificationCode,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default
+    )
+    {
+        await DbContext.UserInfoVerificationCodes.Where(code => code.UserId == verificationCode.UserId && code.CodeType == verificationCode.CodeType)
+            .ExecuteUpdateAsync(setter => setter.SetProperty(code => code.IsActive, false), cancellationToken);
+
+        return await base.CreateAsync(verificationCode, commandOptions, cancellationToken);
+    }
+
+    public async ValueTask DeactivateAsync(Guid codeId, CommandOptions commandOptions, CancellationToken cancellationToken = default)
+    {
+        await DbContext.UserInfoVerificationCodes.Where(code => code.Id == codeId)
+            .ExecuteUpdateAsync(setter => setter.SetProperty(code => code.IsActive, false), cancellationToken);
+
+        if (!commandOptions.SkipSaveChanges)
+            await DbContext.SaveChangesAsync(cancellationToken);
+    }
+}
