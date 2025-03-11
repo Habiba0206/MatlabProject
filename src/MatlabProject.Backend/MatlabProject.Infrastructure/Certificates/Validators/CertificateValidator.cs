79b40f44024fcb484e31c48aa57648a7da8a5dcd
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:41:03 +0500

    Created certificate validator

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Certificates/Validators/CertificateValidator.cs
@@ -0,0 +1,26 @@
+﻿using MatlabProject.Domain.Entities;
+using MatlabProject.Domain.Enums;
+using FluentValidation;
+
+namespace MatlabProject.Infrastructure.Certificates.Validators;
+public class CertificateValidator : AbstractValidator<Certificate>
+{
+    public CertificateValidator()
+    {
+        RuleSet(
+            EntityEvent.OnCreate.ToString(),
+            () =>
+            {
+                RuleFor(certificate => certificate.UserId).NotEqual(Guid.Empty);
+                RuleFor(certificate => certificate.TestId).NotEqual(Guid.Empty);
+                RuleFor(certificate => certificate.FilePath).NotEmpty().MinimumLength(2);
+            });
+
+        RuleSet(
+            EntityEvent.OnUpdate.ToString(),
+            () =>
+            {
+                RuleFor(certificate => certificate.FilePath).NotEmpty().MinimumLength(2);
+            });
+    }
+}
