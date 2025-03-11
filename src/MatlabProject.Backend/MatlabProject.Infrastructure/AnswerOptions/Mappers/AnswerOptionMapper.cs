598f8174ee810667a7c928ea8e499ab4bfc9233d
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:42:21 +0500

    Created answer option mapper

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/AnswerOptions/Mappers/AnswerOptionMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using MatlabProject.Application.AnswerOptions.Models;
+using MatlabProject.Domain.Entities;
+
+namespace MatlabProject.Infrastructure.AnswerOptions.Mappers;
+
+public class AnswerOptionMapper : Profile
+{
+    public AnswerOptionMapper()
+    {
+        CreateMap<AnswerOption, AnswerOptionDto>().ReverseMap();
+    }
+}
