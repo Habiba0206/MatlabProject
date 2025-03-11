a55b7e34d6eadae7ef3d9abe64acd27178816069
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:22:33 +0500

    Created test and test result mappers

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Tests/Mappers/TestMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using MatlabProject.Application.Tests.Models;
+using MatlabProject.Domain.Entities;
+
+namespace MatlabProject.Infrastructure.Tests.Mappers;
+
+public class TestMapper : Profile
+{
+    public TestMapper()
+    {
+        CreateMap<Test, TestDto>().ReverseMap();
+    }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Tests/Mappers/TestResultMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using MatlabProject.Application.Tests.Models;
+using MatlabProject.Domain.Entities;
+
+namespace MatlabProject.Infrastructure.Tests.Mappers;
+
+public class TestResultMapper : Profile
+{
+    public TestResultMapper()
+    {
+        CreateMap<TestResult, TestResultDto>().ReverseMap();
+    }
+}
