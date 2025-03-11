71909a0d12f2bf15429c87d880fe3773961fa9a8
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:39:35 +0500

    Impelemented cache and event bus brokers

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Common/EventBus/Brokers/MassTransitEventBusBroker.cs
@@ -0,0 +1,21 @@
+﻿using MatlabProject.Application.Common.EventBus.Brokers;
+using MatlabProject.Domain.Common.Events;
+using MassTransit;
+using MediatR;
+
+namespace MatlabProject.Infrastructure.Common.EventBus.Brokers;
+
+public class MassTransitEventBusBroker(IBus bus, IPublisher publisher) : IEventBusBroker
+{
+    public async ValueTask PublishAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase =>
+        await publisher.Publish(@event, cancellationToken);
+
+    public async ValueTask PublishLocalAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase =>
+        await bus.Publish(@event, cancellationToken);
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Common/EventBus/Brokers/RabbitMqEventBusBroker.cs
@@ -0,0 +1,20 @@
+﻿using MatlabProject.Application.Common.EventBus.Brokers;
+using MatlabProject.Domain.Common.Events;
+using MediatR;
+
+namespace MatlabProject.Infrastructure.Common.EventBus.Brokers;
+
+public class RabbitMqEventBusBroker(IPublisher publisher) : IEventBusBroker
+{
+    public async ValueTask PublishAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase =>
+    await publisher.Publish(@event, cancellationToken);
+
+    public async ValueTask PublishLocalAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase =>
+    await publisher.Publish(@event, cancellationToken);
+}
