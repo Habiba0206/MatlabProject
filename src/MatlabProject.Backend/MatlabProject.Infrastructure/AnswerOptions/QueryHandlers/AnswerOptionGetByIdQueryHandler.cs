e3eb0301788b0c1ad88c5ba4d5616b33e7472ae4
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:42:10 +0500

    Created answer option query handlers

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/AnswerOptions/QueryHandlers/AnswerOptionGetByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MatlabProject.Application.AnswerOptions.Models;
+using MatlabProject.Application.AnswerOptions.Queries;
+using MatlabProject.Application.AnswerOptions.Services;
+using MatlabProject.Domain.Common.Queries;
+
+namespace MatlabProject.Infrastructure.AnswerOptions.QueryHandlers;
+
+public class AnswerOptionGetByIdQueryHandler(
+    IMapper mapper,
+    IAnswerOptionService answerOptionService)
+    : IQueryHandler<AnswerOptionGetByIdQuery, AnswerOptionDto>
+{
+    public async Task<AnswerOptionDto> Handle(AnswerOptionGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await answerOptionService.GetByIdAsync(request.AnswerOptionId, cancellationToken: cancellationToken);
+
+        return mapper.Map<AnswerOptionDto>(result);
+    }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/AnswerOptions/QueryHandlers/AnswerOptionGetQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MatlabProject.Application.AnswerOptions.Models;
+using MatlabProject.Application.AnswerOptions.Queries;
+using MatlabProject.Application.AnswerOptions.Services;
+using MatlabProject.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatlabProject.Infrastructure.AnswerOptions.QueryHandlers;
+
+public class AnswerOptionGetQueryHandler(
+    IMapper mapper,
+    IAnswerOptionService answerService)
+    : IQueryHandler<AnswerOptionGetQuery, ICollection<AnswerOptionDto>>
+{
+    public async Task<ICollection<AnswerOptionDto>> Handle(AnswerOptionGetQuery request, CancellationToken cancellationToken)
+    {
+        var result = await answerService.Get(
+            request.AnswerOptionFilter,
+            new QueryOptions()
+            {
+                QueryTrackingMode = QueryTrackingMode.AsNoTracking
+            })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<AnswerOptionDto>>(result);
+    }
+}
