21ef0a07e278b8c3eee2c0297797ba0a5cd6b475
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:41:36 +0500

    Created answer option validator

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/AnswerOptions/Validators/AnswerOptionValidator.cs
@@ -0,0 +1,25 @@
+﻿using MatlabProject.Domain.Entities;
+using MatlabProject.Domain.Enums;
+using FluentValidation;
+
+namespace MatlabProject.Infrastructure.AnswerOptions.Validators;
+public class AnswerOptionValidator : AbstractValidator<AnswerOption>
+{
+    public AnswerOptionValidator()
+    {
+        RuleSet(
+            EntityEvent.OnCreate.ToString(),
+            () =>
+            {
+                RuleFor(answerOption => answerOption.QuestionId).NotEqual(Guid.Empty);
+                RuleFor(answerOption => answerOption.Text).NotEmpty().MinimumLength(2);
+            });
+
+        RuleSet(
+            EntityEvent.OnUpdate.ToString(),
+            () =>
+            {
+                RuleFor(answerOption => answerOption.Text).NotEmpty().MinimumLength(2);
+            });
+    }
+}
