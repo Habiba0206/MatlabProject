96f9e01aa4f653145c728a7c97472e7177dadfbc
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:38:13 +0500

    Created user query handlers

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Identity/QueryHandlers/UserGetByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MatlabProject.Application.Identity.Models;
+using MatlabProject.Application.Identity.Queries;
+using MatlabProject.Application.Identity.Services;
+using MatlabProject.Domain.Common.Queries;
+
+namespace MatlabProject.Infrastructure.Identity.QueryHandlers;
+
+public class UserGetByIdQueryHandler(
+    IMapper mapper,
+    IUserService userService)
+    : IQueryHandler<UserGetByIdQuery, UserDto>
+{
+    public async Task<UserDto> Handle(UserGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await userService.GetByIdAsync(request.UserId, cancellationToken: cancellationToken);
+
+        return mapper.Map<UserDto>(result);
+    }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Identity/QueryHandlers/UserGetQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MatlabProject.Application.Identity.Models;
+using MatlabProject.Application.Identity.Queries;
+using MatlabProject.Application.Identity.Services;
+using MatlabProject.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatlabProject.Infrastructure.Identity.QueryHandlers;
+
+public class UserGetQueryHandler(
+    IMapper mapper,
+    IUserService userService)
+    : IQueryHandler<UserGetQuery, ICollection<UserDto>>
+{
+    public async Task<ICollection<UserDto>> Handle(UserGetQuery request, CancellationToken cancellationToken)
+    {
+        var result = await userService.Get(
+            request.UserFilter,
+            new QueryOptions()
+            {
+                QueryTrackingMode = QueryTrackingMode.AsNoTracking
+            })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<UserDto>>(result);
+    }
+}
