02ace0166a5973dee71493f50490adc5d0d07382
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:38:53 +0500

    Created request context provider

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Common/RequestContexts/Brokers/RequestContextProvider.cs
@@ -0,0 +1,29 @@
+﻿using MatlabProject.Domain.Brokers;
+using MatlabProject.Domain.Constants;
+using Microsoft.AspNetCore.Http;
+
+namespace MatlabProject.Infrastructure.Common.RequestContexts.Brokers;
+
+public class RequestContextProvider(IHttpContextAccessor httpContextAccessor) : IRequestContextProvider
+{
+    public Guid GetUserId()
+    {
+        if (!IsLoggedIn())
+            throw new InvalidOperationException("User is not logged in");
+
+        var httpContext = httpContextAccessor.HttpContext;
+        var userIdClaim = httpContext!.User.Claims.First(claim => claim.Type == ClaimConstants.UserId).Value;
+
+        return Guid.Parse(userIdClaim);
+    }
+
+    public string? GetAccessToken()
+    {
+        return httpContextAccessor.HttpContext?.Request.Headers.Authorization;
+    }
+
+    public bool IsLoggedIn()
+    {
+        return httpContextAccessor.HttpContext?.User.Identity?.IsAuthenticated ?? false;
+    }
+}
