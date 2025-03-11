9e0b4bff75f915003b06f509c7fd6454062362b5
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:38:36 +0500

    Created user command handlers

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Identity/CommandHandlers/UserDeleteByIdCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MatlabProject.Application.Identity.Commands;
+using MatlabProject.Application.Identity.Services;
+using MatlabProject.Domain.Common.Commands;
+
+namespace MatlabProject.Infrastructure.Identity.CommandHandlers;
+
+public class UserDeleteByIdCommandHandler(
+    IUserService userService)
+    : ICommandHandler<UserDeleteByIdCommand, bool>
+{
+    public async Task<bool> Handle(UserDeleteByIdCommand request, CancellationToken cancellationToken)
+    {
+        var result = await userService.DeleteByIdAsync(request.UserId, cancellationToken: cancellationToken);
+
+        return result is not null;
+    }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Identity/CommandHandlers/UserUpdateCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MatlabProject.Application.Identity.Commands;
+using MatlabProject.Application.Identity.Models;
+using MatlabProject.Application.Identity.Services;
+using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Entities;
+
+namespace MatlabProject.Infrastructure.Identity.CommandHandlers;
+
+public class UserUpdateCommandHandler(
+    IMapper mapper,
+    IUserService userService) : ICommandHandler<UserUpdateCommand, UserDto>
+{
+    public async Task<UserDto> Handle(UserUpdateCommand request, CancellationToken cancellationToken)
+    {
+        var user = mapper.Map<User>(request.UserDto);
+
+        var updatedUser = await userService.UpdateAsync(user, cancellationToken: cancellationToken);
+
+        return mapper.Map<UserDto>(updatedUser);
+    }
+}
