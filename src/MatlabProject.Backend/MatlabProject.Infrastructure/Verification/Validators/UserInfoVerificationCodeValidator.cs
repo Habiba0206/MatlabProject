78e623c3c4fda9d52c097428c10f3a7c8796155b
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:07:05 +0500

    Created user info verification validator

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Verification/Validators/UserInfoVerificationCodeValidator.cs
@@ -0,0 +1,32 @@
+﻿using MatlabProject.Application.Common.Settings;
+using MatlabProject.Domain.Entities;
+using MatlabProject.Domain.Enums;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace MatlabProject.Infrastructure.Verification.Validators;
+
+public class UserInfoVerificationCodeValidator : AbstractValidator<UserInfoVerificationCode>
+{
+    public UserInfoVerificationCodeValidator(IOptions<VerificationSettings> verificationSettings, IOptions<ValidationSettings> validationSettings)
+    {
+        var verificationSettingsValue = verificationSettings.Value;
+        var validationSettingsValue = validationSettings.Value;
+
+        RuleSet(
+            EntityEvent.OnCreate.ToString(),
+            () =>
+            {
+                RuleFor(code => code.UserId).NotEqual(Guid.Empty);
+
+                RuleFor(code => code.ExpiryTime)
+                    .GreaterThanOrEqualTo(DateTimeOffset.UtcNow)
+                    .LessThanOrEqualTo(DateTimeOffset.UtcNow.AddSeconds(verificationSettingsValue.VerificationCodeExpiryTimeInSeconds));
+
+                RuleFor(code => code.IsActive).Equal(true);
+
+                RuleFor(code => code.VerificationLink).NotEmpty().Matches(validationSettingsValue.UrlRegexPattern);
+            }
+        );
+    }
+}
