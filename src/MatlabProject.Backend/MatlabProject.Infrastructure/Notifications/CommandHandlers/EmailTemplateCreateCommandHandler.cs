d11bd9648199c5a4d59d8fc009577a16c0f7ac96
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:34:57 +0500

    Created email template create command handler

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Notifications/CommandHandlers/EmailTemplateCreateCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MatlabProject.Application.Identity.Commands;
+using MatlabProject.Application.Identity.Models;
+using MatlabProject.Application.Identity.Services;
+using MatlabProject.Application.Notifications.Commands;
+using MatlabProject.Application.Notifications.Models;
+using MatlabProject.Application.Notifications.Services;
+using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Entities;
+
+namespace MatlabProject.Infrastructure.Notifications.CommandHandlers;
+
+public class EmailTemplateCreateCommandHandler(
+    IMapper mapper,
+    IEmailTemplateService emailTemplateService) : ICommandHandler<EmailTemplateCreateCommand, EmailTemplateDto>
+{
+    public async Task<EmailTemplateDto> Handle(EmailTemplateCreateCommand request, CancellationToken cancellationToken)
+    {
+        var emailTemplate = mapper.Map<EmailTemplate>(request.EmailTemplateDto);
+
+        var createdEmailTemplate = await emailTemplateService.CreateAsync(emailTemplate, cancellationToken: cancellationToken);
+
+        return mapper.Map<EmailTemplateDto>(createdEmailTemplate);
+    }
+}
