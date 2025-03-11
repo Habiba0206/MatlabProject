4604765b5f43df7c4345f33ec973dd895ebabcf3
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:07:56 +0500

    Implemented test and test result services

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Tests/Services/TestResultService.cs
@@ -0,0 +1,94 @@
+﻿using MatlabProject.Application.Tests.Models;
+using MatlabProject.Application.Tests.Services;
+using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Domain.Entities;
+using MatlabProject.Domain.Enums;
+using MatlabProject.Persistence.Extensions;
+using MatlabProject.Persistence.Repositories.Interfaces;
+using FluentValidation;
+using System.Linq.Expressions;
+using MatlabProject.Infrastructure.Tests.Validators;
+
+namespace MatlabProject.Infrastructure.Tests.Services;
+
+public class TestResultService(
+    ITestResultRepository testResultRepository,
+    TestResultValidator validator)
+   : ITestResultService
+{
+    public IQueryable<TestResult> Get(
+        Expression<Func<TestResult, bool>>? predicate = null,
+        QueryOptions queryOptions = default) =>
+    testResultRepository.Get(predicate, queryOptions);
+
+    public IQueryable<TestResult> Get(
+        TestResultFilter testResultFilter,
+        QueryOptions queryOptions = default) =>
+    testResultRepository
+        .Get(queryOptions: queryOptions)
+        .ApplyPagination(testResultFilter);
+
+    public ValueTask<TestResult?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    testResultRepository.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<TestResult>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    testResultRepository.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+    testResultRepository.CheckByIdAsync(id, cancellationToken);
+
+    public async ValueTask<TestResult> CreateAsync(
+        TestResult testResult,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default)
+    {
+        var validationResult = await validator.ValidateAsync(
+            testResult,
+            options => options
+            .IncludeRuleSets(EntityEvent.OnCreate.ToString()),
+            cancellationToken);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        return await testResultRepository.CreateAsync(testResult, commandOptions, cancellationToken);
+    }
+
+    public async ValueTask<TestResult> UpdateAsync(
+        TestResult testResult,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default)
+    {
+        var validationResult = await validator.ValidateAsync(
+            testResult,
+            options => options
+            .IncludeRuleSets(EntityEvent.OnUpdate.ToString()),
+            cancellationToken);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        return await testResultRepository.UpdateAsync(testResult, commandOptions, cancellationToken);
+    }
+
+    public ValueTask<TestResult?> DeleteAsync(
+        TestResult testResult,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default) =>
+    testResultRepository.DeleteAsync(testResult, commandOptions, cancellationToken);
+
+    public ValueTask<TestResult?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default) =>
+    testResultRepository.DeleteByIdAsync(id, commandOptions, cancellationToken);
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Tests/Services/TestService.cs
@@ -0,0 +1,96 @@
+﻿using MatlabProject.Application.Tests.Models;
+using MatlabProject.Application.Tests.Services;
+using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Domain.Entities;
+using MatlabProject.Domain.Enums;
+using MatlabProject.Persistence.Extensions;
+using MatlabProject.Persistence.Repositories.Interfaces;
+using FluentValidation;
+using MatlabProject.Infrastructure.Tests.Validators;
+using System.Linq.Expressions;
+
+namespace MatlabProject.Infrastructure.Tests.Services;
+
+public class TestService(
+    ITestRepository testRepository,
+    TestValidator validator)
+   : ITestService
+{
+    public IQueryable<Test> Get(
+        Expression<Func<Test, bool>>? predicate = null,
+        QueryOptions queryOptions = default)
+    {
+        return testRepository.Get(predicate, queryOptions);
+    }
+
+    public IQueryable<Test> Get(
+        TestFilter testFilter,
+        QueryOptions queryOptions = default) =>
+    testRepository
+        .Get(queryOptions: queryOptions)
+        .ApplyPagination(testFilter);
+
+    public ValueTask<Test?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    testRepository.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<Test>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    testRepository.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+    testRepository.CheckByIdAsync(id, cancellationToken);
+
+    public async ValueTask<Test> CreateAsync(
+        Test test,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default)
+    {
+        var validationResult = await validator.ValidateAsync(
+            test,
+            options => options
+            .IncludeRuleSets(EntityEvent.OnCreate.ToString()),
+            cancellationToken);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        return await testRepository.CreateAsync(test, commandOptions, cancellationToken);
+    }
+
+    public async ValueTask<Test> UpdateAsync(
+        Test test,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default)
+    {
+        var validationResult = await validator.ValidateAsync(
+            test,
+            options => options
+            .IncludeRuleSets(EntityEvent.OnUpdate.ToString()),
+            cancellationToken);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        return await testRepository.UpdateAsync(test, commandOptions, cancellationToken);
+    }
+
+    public ValueTask<Test?> DeleteAsync(
+        Test test,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default) =>
+    testRepository.DeleteAsync(test, commandOptions, cancellationToken);
+
+    public ValueTask<Test?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default) =>
+    testRepository.DeleteByIdAsync(id, commandOptions, cancellationToken);
+}
