4904788819401768ce83dc72e0c5b93549d13732
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:40:36 +0500

    Created certificate query handlers

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Certificates/QueryHandlers/CertificateGetByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MatlabProject.Application.Certificates.Models;
+using MatlabProject.Application.Certificates.Queries;
+using MatlabProject.Application.Certificates.Services;
+using MatlabProject.Domain.Common.Queries;
+
+namespace MatlabProject.Infrastructure.Certificates.QueryHandlers;
+
+public class CertificateGetByIdQueryHandler(
+    IMapper mapper,
+    ICertificateService certificateService)
+    : IQueryHandler<CertificateGetByIdQuery, CertificateDto>
+{
+    public async Task<CertificateDto> Handle(CertificateGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await certificateService.GetByIdAsync(request.CertificateId, cancellationToken: cancellationToken);
+
+        return mapper.Map<CertificateDto>(result);
+    }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Certificates/QueryHandlers/CertificateGetQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MatlabProject.Application.Certificates.Models;
+using MatlabProject.Application.Certificates.Queries;
+using MatlabProject.Application.Certificates.Services;
+using MatlabProject.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatlabProject.Infrastructure.Certificates.QueryHandlers;
+
+public class CertificateGetQueryHandler(
+    IMapper mapper,
+    ICertificateService certificateService)
+    : IQueryHandler<CertificateGetQuery, ICollection<CertificateDto>>
+{
+    public async Task<ICollection<CertificateDto>> Handle(CertificateGetQuery request, CancellationToken cancellationToken)
+    {
+        var result = await certificateService.Get(
+            request.CertificateFilter,
+            new QueryOptions()
+            {
+                QueryTrackingMode = QueryTrackingMode.AsNoTracking
+            })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<CertificateDto>>(result);
+    }
+}
