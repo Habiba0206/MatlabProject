ec602f04ae614ab40a7128ca2c5fb3d414a6f869
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:38:24 +0500

    Created user mapper

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Identity/Mappers/UserMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MatlabProject.Application.Identity.Models;
+using MatlabProject.Domain.Entities;
+
+namespace MatlabProject.Infrastructure.Identity.Mappers;
+
+public class UserMapper : Profile
+{
+    public UserMapper()
+    {
+        CreateMap<User, UserDto>().ReverseMap();
+        CreateMap<SignInDetails, User>().ReverseMap();
+        CreateMap<SignUpDetails, User>().ReverseMap();
+    }
+}
