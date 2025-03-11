9794f69b6e76e52d611ffef5d2d2510068d67527
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:25:25 +0500

    created question query handlers

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Questions/QueryHandlers/QuestionGetByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MatlabProject.Application.Questions.Models;
+using MatlabProject.Application.Questions.Queries;
+using MatlabProject.Application.Questions.Services;
+using MatlabProject.Domain.Common.Queries;
+
+namespace MatlabProject.Infrastructure.Questions.QueryHandlers;
+
+public class QuestionGetByIdQueryHandler(
+    IMapper mapper,
+    IQuestionService questionService)
+    : IQueryHandler<QuestionGetByIdQuery, QuestionDto>
+{
+    public async Task<QuestionDto> Handle(QuestionGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await questionService.GetByIdAsync(request.QuestionId, cancellationToken: cancellationToken);
+
+        return mapper.Map<QuestionDto>(result);
+    }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Questions/QueryHandlers/QuestionGetQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MatlabProject.Application.Questions.Models;
+using MatlabProject.Application.Questions.Queries;
+using MatlabProject.Application.Questions.Services;
+using MatlabProject.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatlabProject.Infrastructure.Questions.QueryHandlers;
+
+public class QuestionGetQueryHandler(
+    IMapper mapper,
+    IQuestionService questionService)
+    : IQueryHandler<QuestionGetQuery, ICollection<QuestionDto>>
+{
+    public async Task<ICollection<QuestionDto>> Handle(QuestionGetQuery request, CancellationToken cancellationToken)
+    {
+        var result = await questionService.Get(
+            request.QuestionFilter,
+            new QueryOptions()
+            {
+                QueryTrackingMode = QueryTrackingMode.AsNoTracking
+            })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<QuestionDto>>(result);
+    }
+}
