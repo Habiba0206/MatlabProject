5f94d4a9e511c58c4256a2bd9ce120b02a6ab33e
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:07:39 +0500

    Created test and test result validators

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Tests/Validators/TestResultValidator.cs
@@ -0,0 +1,13 @@
+﻿using MatlabProject.Domain.Entities;
+using MatlabProject.Domain.Enums;
+using FluentValidation;
+
+namespace MatlabProject.Infrastructure.Tests.Validators;
+public class TestResultValidator : AbstractValidator<TestResult>
+{
+    public TestResultValidator()
+    {
+        RuleFor(testResult => testResult.UserId).NotEqual(Guid.Empty);
+        RuleFor(testResult => testResult.TestId).NotEqual(Guid.Empty);
+    }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Tests/Validators/TestValidator.cs
@@ -0,0 +1,13 @@
+﻿using MatlabProject.Domain.Entities;
+using MatlabProject.Domain.Enums;
+using FluentValidation;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MatlabProject.Infrastructure.Tests.Validators;
+public class TestValidator : AbstractValidator<Test>
+{
+    public TestValidator()
+    {
+        RuleFor(test => test.Title).NotEmpty().MinimumLength(2);
+    }
+}
