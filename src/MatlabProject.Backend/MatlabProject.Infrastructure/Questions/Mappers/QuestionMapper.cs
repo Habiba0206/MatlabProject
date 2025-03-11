46bb573eb87f5e49a811b31fd1a198dc53524854
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:26:40 +0500

    Created question mapper

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Questions/Mappers/QuestionMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using MatlabProject.Application.Questions.Models;
+using MatlabProject.Domain.Entities;
+
+namespace MatlabProject.Infrastructure.Questions.Mappers;
+
+public class QuestionMapper : Profile
+{
+    public QuestionMapper()
+    {
+        CreateMap<Question, QuestionDto>().ReverseMap();
+    }
+}
