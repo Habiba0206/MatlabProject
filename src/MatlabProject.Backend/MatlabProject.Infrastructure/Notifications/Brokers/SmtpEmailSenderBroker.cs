2b48ec03a3379929abdb076678ea2dfb6b446ac4
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:35:18 +0500

    Implemented smtp email sender broker

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Notifications/Brokers/SmtpEmailSenderBroker.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Mail;
+using MatlabProject.Application.Common.Settings;
+using MatlabProject.Application.Notifications.Brokers;
+using MatlabProject.Application.Notifications.Models;
+using Microsoft.Extensions.Options;
+
+namespace MatlabProject.Infrastructure.Notifications.Brokers;
+
+public class SmtpEmailSenderBroker : IEmailSenderBroker
+{
+    private readonly SmtpEmailSenderSettings _smtpEmailSenderSettings;
+
+    public SmtpEmailSenderBroker(IOptions<SmtpEmailSenderSettings> smtpEmailSenderSettings)
+    {
+        _smtpEmailSenderSettings = smtpEmailSenderSettings.Value;
+    }
+
+    public ValueTask<bool> SendAsync(EmailMessage emailMessage, CancellationToken cancellationToken = default)
+    {
+        var mail = new MailMessage(emailMessage.SenderEmailAddress, emailMessage.ReceiverEmailAddress);
+        mail.Subject = emailMessage.Subject;
+        mail.Body = emailMessage.Body;
+        mail.IsBodyHtml = true;
+
+        var smtpClient = new SmtpClient(_smtpEmailSenderSettings.Host, _smtpEmailSenderSettings.Port);
+        smtpClient.Credentials = new NetworkCredential(_smtpEmailSenderSettings.CredentialAddress, _smtpEmailSenderSettings.Password);
+        smtpClient.EnableSsl = true;
+
+        smtpClient.Send(mail);
+
+        return new ValueTask<bool>(true);
+    }
+}
