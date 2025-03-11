cf0ac6628f549905cfefb8890a75e055139ca252
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:23:13 +0500

    Created question validator

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Questions/Validators/QuestionValidator.cs
@@ -0,0 +1,28 @@
+﻿using MatlabProject.Domain.Entities;
+using MatlabProject.Domain.Enums;
+using FluentValidation;
+
+namespace MatlabProject.Infrastructure.Questions.Validators;
+
+public class QuestionValidator : AbstractValidator<Question>
+{
+    public QuestionValidator()
+    {
+        RuleSet(
+            EntityEvent.OnCreate.ToString(),
+            () =>
+            {
+                RuleFor(question => question.TestId).NotEqual(Guid.Empty);
+                RuleFor(question => question.Text).NotEmpty().MaximumLength(2);
+                RuleFor(question => question.CorrectAnswer).NotEmpty().MaximumLength(2);
+            });
+
+        RuleSet(
+            EntityEvent.OnUpdate.ToString(),
+            () =>
+            {
+                RuleFor(question => question.Text).NotEmpty().MaximumLength(2);
+                RuleFor(question => question.CorrectAnswer).NotEmpty().MaximumLength(2);
+            });
+    }
+}
