e95fb70e4ca833be3ac4f900646665c302c5d092
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-11 15:41:23 +0500

    Created certificate mapper

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/Certificates/Mappers/CertificateMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using MatlabProject.Application.Certificates.Models;
+using MatlabProject.Domain.Entities;
+
+namespace MatlabProject.Infrastructure.Certificates.Mappers;
+
+public class CertificateMapper : Profile
+{
+    public CertificateMapper()
+    {
+        CreateMap<Certificate, CertificateDto>().ReverseMap();
+    }
+}
