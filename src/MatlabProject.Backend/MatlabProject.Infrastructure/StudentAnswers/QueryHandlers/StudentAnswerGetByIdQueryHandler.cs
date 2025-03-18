d6056eea0eaf2fcd4c7343cc87dd79b25484f35b
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-18 13:56:38 +0500

    Created student answer query handlers

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/StudentAnswers/QueryHandlers/StudentAnswerGetByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MatlabProject.Application.StudentAnswers.Models;
+using MatlabProject.Application.StudentAnswers.Queries;
+using MatlabProject.Application.StudentAnswers.Services;
+using MatlabProject.Domain.Common.Queries;
+
+namespace MatlabProject.Infrastructure.StudentAnswers.QueryHandlers;
+
+public class StudentAnswerGetByIdQueryHandler(
+    IMapper mapper,
+    IStudentAnswerService studentAnswerService)
+    : IQueryHandler<StudentAnswerGetByIdQuery, StudentAnswerDto>
+{
+    public async Task<StudentAnswerDto> Handle(StudentAnswerGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await studentAnswerService.GetByIdAsync(request.StudentAnswerId, cancellationToken: cancellationToken);
+
+        return mapper.Map<StudentAnswerDto>(result);
+    }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/StudentAnswers/QueryHandlers/StudentAnswerGetQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MatlabProject.Application.StudentAnswers.Models;
+using MatlabProject.Application.StudentAnswers.Queries;
+using MatlabProject.Application.StudentAnswers.Services;
+using MatlabProject.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatlabProject.Infrastructure.StudentAnswers.QueryHandlers;
+
+public class StudentAnswerGetQueryHandler(
+    IMapper mapper,
+    IStudentAnswerService studentAnswerService)
+    : IQueryHandler<StudentAnswerGetQuery, ICollection<StudentAnswerDto>>
+{
+    public async Task<ICollection<StudentAnswerDto>> Handle(StudentAnswerGetQuery request, CancellationToken cancellationToken)
+    {
+        var result = await studentAnswerService.Get(
+            request.StudentAnswerFilter,
+            new QueryOptions()
+            {
+                QueryTrackingMode = QueryTrackingMode.AsNoTracking
+            })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<StudentAnswerDto>>(result);
+    }
+}
