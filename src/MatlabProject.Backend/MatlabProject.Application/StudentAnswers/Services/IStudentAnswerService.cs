d954e2d2ea53078afeeb158066391036c95091b8
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-18 12:03:35 +0500

    Created student answer service

---
+++ src/MatlabProject.Backend/MatlabProject.Application/StudentAnswers/Services/IStudentAnswerService.cs
@@ -0,0 +1,52 @@
+﻿using MatlabProject.Application.StudentAnswers.Models;
+using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace MatlabProject.Application.StudentAnswers.Services;
+
+public interface IStudentAnswerService
+{
+    IQueryable<StudentAnswer> Get(
+             Expression<Func<StudentAnswer, bool>>? predicate = default,
+             QueryOptions queryOptions = default);
+
+    IQueryable<StudentAnswer> Get(
+        StudentAnswerFilter studentAnswerFilter,
+        QueryOptions queryOptions = default);
+
+    ValueTask<StudentAnswer?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<IList<StudentAnswer>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<StudentAnswer> CreateAsync(
+        StudentAnswer studentAnswer,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<StudentAnswer> UpdateAsync(
+        StudentAnswer studentAnswer,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<StudentAnswer?> DeleteAsync(
+        StudentAnswer studentAnswer,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<StudentAnswer?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+}
