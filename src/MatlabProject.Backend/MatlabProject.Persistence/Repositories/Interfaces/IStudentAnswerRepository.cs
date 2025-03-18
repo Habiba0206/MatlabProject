351ac1febd565226ef9bd23c0a3694205ea4e259
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-18 11:33:19 +0500

    Created and implemented student answer repositories

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/Repositories/Interfaces/IStudentAnswerRepository.cs
@@ -0,0 +1,47 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace MatlabProject.Persistence.Repositories.Interfaces;
+
+public interface IStudentAnswerRepository
+{
+    IQueryable<StudentAnswer> Get(
+             Expression<Func<StudentAnswer, bool>>? predicate = default,
+             QueryOptions queryOptions = default);
+
+    ValueTask<StudentAnswer?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<IList<StudentAnswer>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<StudentAnswer> CreateAsync(
+        StudentAnswer studentAnswer,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<StudentAnswer> UpdateAsync(
+        StudentAnswer studentAnswer,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<StudentAnswer?> DeleteAsync(
+        StudentAnswer studentAnswer,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<StudentAnswer?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/Repositories/StudenAnswerRepository.cs
@@ -0,0 +1,61 @@
+﻿using MatlabProject.Domain.Common.Commands;
+using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Domain.Entities;
+using MatlabProject.Persistence.Caching.Brokers;
+using MatlabProject.Persistence.DataContexts;
+using MatlabProject.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace MatlabProject.Persistence.Repositories;
+
+public class StudenAnswerRepository(AppDbContext appDbContext, ICacheBroker cacheBroker) :
+    EntityRepositoryBase<StudentAnswer, AppDbContext>(appDbContext, cacheBroker),
+    IStudentAnswerRepository
+
+{
+    public IQueryable<StudentAnswer> Get(
+        Expression<Func<StudentAnswer, bool>>? predicate = null,
+        QueryOptions queryOptions = default) =>
+    base.Get(predicate, queryOptions);
+
+    public ValueTask<StudentAnswer?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<StudentAnswer>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+    base.CheckByIdAsync(id, cancellationToken);
+
+    public ValueTask<StudentAnswer> CreateAsync(
+        StudentAnswer studentAnswer,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.CreateAsync(studentAnswer, commandOptions, cancellationToken);
+
+    public ValueTask<StudentAnswer> UpdateAsync(
+        StudentAnswer studentAnswer,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken) =>
+    base.UpdateAsync(studentAnswer, commandOptions, cancellationToken);
+
+    public ValueTask<StudentAnswer?> DeleteAsync(
+        StudentAnswer studentAnswer,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default) =>
+    base.UpdateAsync(studentAnswer, commandOptions, cancellationToken);
+
+    public ValueTask<StudentAnswer?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default) =>
+    base.DeleteByIdAsync(id, commandOptions, cancellationToken);
+}
