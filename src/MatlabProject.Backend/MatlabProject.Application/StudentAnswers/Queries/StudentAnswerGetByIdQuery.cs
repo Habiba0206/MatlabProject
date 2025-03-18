fd5ddd2e1da594c2d77cdcd98ca4f4c952f5a905
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-18 12:03:46 +0500

    Created student answer queries

---
+++ src/MatlabProject.Backend/MatlabProject.Application/StudentAnswers/Queries/StudentAnswerGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Application.StudentAnswers.Models;
+
+namespace MatlabProject.Application.StudentAnswers.Queries;
+
+public class StudentAnswerGetByIdQuery : IQuery<StudentAnswerDto?>
+{
+    public Guid StudentAnswerId { get; set; }
+}
---
+++ src/MatlabProject.Backend/MatlabProject.Application/StudentAnswers/Queries/StudentAnswerGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using MatlabProject.Domain.Common.Queries;
+using MatlabProject.Application.StudentAnswers.Models;
+
+namespace MatlabProject.Application.StudentAnswers.Queries;
+
+public class StudentAnswerGetQuery : IQuery<ICollection<StudentAnswerDto>>
+{
+    public StudentAnswerFilter StudentAnswerFilter { get; set; }
+}
