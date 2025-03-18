121c79af7cbbe9b0efdeeb3273d5453e9a98dec3
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-18 11:32:15 +0500

    Added new entity

---
+++ src/MatlabProject.Backend/MatlabProject.Domain/Entities/StudentAnswer.cs
@@ -0,0 +1,13 @@
+﻿using MatlabProject.Domain.Common.Entities;
+
+namespace MatlabProject.Domain.Entities;
+
+public class StudentAnswer : AuditableEntity
+{
+    public Guid TestResultId { get; set; }
+    public Guid QuestionId { get; set; }
+    public string SelectedAnswer { get; set; } = default!;
+    public bool IsCorrect { get; set; }
+    public TestResult TestResult { get; set; }
+    public  Question Question { get; set; }
+}
