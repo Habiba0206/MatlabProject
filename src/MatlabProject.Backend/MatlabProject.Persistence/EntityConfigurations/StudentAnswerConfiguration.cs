10acdd73c12eb9fb26eb2d17a7596a2f63666f83
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-18 11:32:46 +0500

    Added new student answer configuration

---
+++ src/MatlabProject.Backend/MatlabProject.Persistence/EntityConfigurations/StudentAnswerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using MatlabProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MatlabProject.Persistence.EntityConfigurations;
+
+public class StudentAnswerConfiguration : IEntityTypeConfiguration<StudentAnswer>
+{
+    public void Configure(EntityTypeBuilder<StudentAnswer> builder)
+    {
+        builder.Property(sa => sa.SelectedAnswer)
+            .IsRequired();
+
+        builder.Property(sa => sa.IsCorrect)
+            .IsRequired();
+
+        builder.HasOne(sa => sa.TestResult)
+            .WithMany(tr => tr.StudentAnswers)
+            .HasForeignKey(sa => sa.TestResultId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
