80d975207010b76b807d983a18bad40b6840b39c
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-18 13:56:49 +0500

    Created student answer mappers

---
+++ src/MatlabProject.Backend/MatlabProject.Infrastructure/StudentAnswers/Mappers/StudentAnswerMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using MatlabProject.Application.StudentAnswers.Models;
+using MatlabProject.Domain.Entities;
+
+namespace MatlabProject.Infrastructure.StudentAnswers.Mappers;
+
+public class StudentAnswerMapper : Profile
+{
+    public StudentAnswerMapper()
+    {
+        CreateMap<StudentAnswer, StudentAnswerDto>().ReverseMap();
+    }
+}
