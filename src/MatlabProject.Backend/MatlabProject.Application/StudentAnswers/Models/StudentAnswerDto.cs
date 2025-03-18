45c639e443671beac811303ca269f70c631e2472
Habiba Sattorova <habibasattorova26@gmail.com>
2025-03-18 12:04:11 +0500

    Created student answer dto model

---
+++ src/MatlabProject.Backend/MatlabProject.Application/StudentAnswers/Models/StudentAnswerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MatlabProject.Application.StudentAnswers.Models;
+
+public class StudentAnswerDto
+{
+    public Guid? Id { get; set; }
+    public Guid FormId { get; set; }
+    public Guid QuestionId { get; set; }
+    public string Value { get; set; }
+}
